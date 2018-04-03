c5dd19abf369aa032813859b733625966d48d2d7
Nathan Gostelow <nathan-gostelow-94@hotmail.com>
2018-04-03 21:31:03 +0100

    testing routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link} from '../node_modules/react-router-dom';
+
 import Menu from './Menu/Menu.js';
 import logo2 from './logo.png';
 import './App.css';
@@ -6,14 +8,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo2} className="App-logo" alt="logo" />
-        </header>
+      <Router>
+        <div className="App">
+          
+          <header className="App-header">
+            <img src={logo2} className="App-logo" alt="logo" />
+          </header>
 
-      <Menu/>
+        <Menu/>
 
-      </div>
+        </div>
+      </Router>
     );
   }
 }
