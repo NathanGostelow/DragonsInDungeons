c03d1bc522ff158e7ca7bf71b88ca084f251fcfa
Nathan Gostelow <11179684+NathanGostelow@users.noreply.github.com>
2018-04-04 20:18:09 +0100

    refactored menu to retrieve props

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -3,17 +3,31 @@ import './Menu.css';
 import Card from './Card/Card.js';
 
 class Menu extends Component {
+  renderCard(i){
+  	return(
+  		<Card
+  			value={this.props.value[i]}
+  			img={this.props.icon[i]}
+  		/>
+  	);
+  }
   render() {
     return (
       <div className="Menu">
         <div className="Row">
-	        <Card value={'Spells'} img={'fa-bolt'} />
+        	{this.renderCard(0)}
+        	{this.renderCard(1)}
+        	{this.renderCard(2)}
+        	{this.renderCard(3)}
+        	{this.renderCard(4)}
+        	{this.renderCard(5)}
+	       {/* <Card value={'Spells'} img={'fa-bolt'} />
 	        <Card value={'Classes'} img={'fa-medkit'} />
 	        <Card value={'Monsters'} img={'fa-gitlab'} />
 
 	        <Card value={'SubClasses'} img={'fa-balance-scale'} />
 	        <Card value={'Races'} img={'fa-hand-spock-o'} />
-	        <Card value={'Equipment'} img={'fa-apple'} />
+	        <Card value={'Equipment'} img={'fa-apple'} />*/}
         </div>
 
         
