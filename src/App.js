e9bf92d4fd55067fe35e29c9a106c4b478711946
Nathan <nathan.gostelow@soprasteria.com>
2018-04-04 20:18:09 +0100

    refactored menu to retrieve props

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    const value = [
+    'Spells', 
+    'Classes', 
+    'Monsters', 
+    'SubClasses',
+    'Races', 
+    'Equipment'
+    ];
+
+    const icon = [
+      'fa-bolt',
+      'fa-medkit',
+      'fa-gitlab',
+      'fa-balance-scale',
+      'fa-hand-spock-o',
+      'fa-apple',
+    ]
+
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo2} className="App-logo" alt="logo" />
         </header>
 
-      <Menu/>
+      <Menu
+        value={value}
+        icon={icon}
+      />
 
       </div>
     );
