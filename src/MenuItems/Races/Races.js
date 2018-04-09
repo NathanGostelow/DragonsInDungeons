46baa0006c59440b11d21b8bf80224195f0e8c94
Nathan Gostelow <11179684+NathanGostelow@users.noreply.github.com>
2018-04-09 20:44:31 +0100

    sorted data and removed duplicates

--- src/MenuItems/Races/Races.js
+++ src/MenuItems/Races/Races.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
-// import './Races.css';
-// import Card from './Card/Card.js';
+import D20 from '../D20.png';
 
 class Races extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			error: null,
+			races : [],
+			isLoaded: false
+		};
+	}
+
+	componentDidMount() {
+    this.RacesList();
+  }
+
+  RacesList() {
+    fetch('http://www.dnd5eapi.co/api/races')
+    	.then(res => res.json())
+      .then(
+      	(result) => {
+          result.results.sort((a, b) => a.name - b.name);
+    	  	this.setState({
+		     		isLoaded: true,
+   	   			races: result.results.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)) 
+
+   	   		})
+      },
+      (error) =>{
+      	this.setState({
+      		isLoaded: true,
+      		error
+      	})
+      });
+  }
+
   render() {
-    return (
-      <div className="container">
-        <h1> Hello Races </h1>        
-      </div>
-    );
+  	const {error, isLoaded, races} = this.state;
+  	if(error){
+  		return <div> Error: {error.message}</div>
+  	} else if(!isLoaded){
+  		return <img src={D20} className="loading-image" alt="Loading" />
+  	} else {
+	    return (
+	      <div className="container">
+	        <h1> Races </h1>
+	        	{races.map(item => (
+	        		<div className='equipment-card' key={item.name}>
+	        			<span className='equipment-title'>{item.name}</span>
+	        		</div>
+	        	))}       
+	      </div>
+	    );
+	  }
   }
 }
 
--- src/MenuItems/Subclasses/Subclasses.js
+++ src/MenuItems/Subclasses/Subclasses.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
-// import './Subclasses.css';
-// import Card from './Card/Card.js';
+import _ from 'lodash';
+import D20 from '../D20.png';
 
-class Subclasses extends Component {
+class SubClasses extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			error: null,
+			subclasses : [],
+			isLoaded: false
+		};
+	}
+
+	componentDidMount() {
+    this.SubClasslist();
+  }
+
+  SubClasslist() {
+    fetch('http://www.dnd5eapi.co/api/subclasses')
+    	.then(res => res.json())
+      .then(
+      	(result) => {
+    	  	this.setState({
+		     		isLoaded: true,
+            subclasses: _.uniqBy(result.results, 'name')
+   	   		})
+          this.SortItems();
+          // this.AddToGlossary();
+      },
+      (error) =>{
+      	this.setState({
+      		isLoaded: true,
+      		error
+      	})
+      });
+  }
+
+  SortItems(){
+    this.setState({subclasses: this.state.subclasses.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))}) 
+  }
+
+  // AddToGlossary(){
+  //   const alph = 'abcdefghijklmnopqrstuvqxyz'.split('');
+  // }
+
   render() {
-    return (
-      <div className="container">
-        <h1> Hello Subclasses </h1>        
-      </div>
-    );
+  	const {error, isLoaded, subclasses} = this.state;
+  	if(error){
+  		return <div> Error: {error.message}</div>
+  	} else if(!isLoaded){
+  		return <img src={D20} className="loading-image" alt="Loading" />
+  	} else {
+	    return (
+	      <div className="container">
+	        <h1> SubClasses </h1>
+	        	{subclasses.map(item => (
+	        		<div className='equipment-card' key={item.name}>
+	        			<span className='equipment-title'>{item.name}</span>
+	        		</div>
+	        	))}       
+	      </div>
+	    );
+	  }
   }
 }
 
-export default Subclasses;
+export default SubClasses;
