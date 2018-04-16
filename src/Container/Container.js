f4b8dc160191a021b37514aecf227c23ba1322dd
Nathan <nathan-gostelow-94@hotmail.com>
2018-04-16 07:03:15 +0100

    removed subclass as i dont know what to do with it for the time being

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -3,6 +3,7 @@ import {Switch, BrowserRouter, Route, Link} from 'react-router-dom';
 import Subclasses from '../MenuItems/Subclasses/Subclasses.js';
 import Classes from '../MenuItems/Classes/Classes.js';
 import Races from '../MenuItems/Races/Races.js';
+import RaceClass from '../MenuItems/RaceClass/RaceClass.js';
 import Equipment from '../MenuItems/Equipment/Equipment.js';
 import logo2 from '../logo.png';
 import '../App.css';
@@ -17,9 +18,9 @@ class Container extends Component {
           <Route path='/monsters' render={() => <Equipment cont='monsters'/>} />
           <Route path='/equipment' render={() => <Equipment cont='equipment'/>} />
           <Route path='/spells' render={() => <Equipment cont='spells'/>} />
-          <Route path='/subclasses' component={Subclasses} />
-          <Route path='/classes' component={Classes} />
-          <Route path='/races' component={Races} />
+          <Route path='/classes' render={() => <RaceClass cont='classes'/>} />
+          <Route path='/races' render={() => <RaceClass cont='races'/>} />
+          {/*<Route path='/subclasses' component={Subclasses} />*/}
         </Switch>
       </main>
     );
