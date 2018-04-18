18c29961f4c7ba75e0dd7b5928196af420f26be6
Nathan Gostelow <11179684+NathanGostelow@users.noreply.github.com>
2018-04-18 21:57:26 +0100

    fixed state on individual item cards

--- src/MenuItems/Equipment/Monster/Monster.js
+++ src/MenuItems/Equipment/Monster/Monster.js
@@ -4,13 +4,57 @@ import D20 from '../../D20.png';
 // import Card from './Card/Card.js';
 
 class Monster extends Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+	  	item: null,
+	  	isLoaded: false
+	  };
+
+	}
 
   componentDidMount(){
-    console.log(this.props);
+  	console.log(this.props);
+    this.EquipmentList();
+  }
+
+  EquipmentList() {
+  	const url = 'http://www.dnd5eapi.co/api/equipment/';
+    fetch( url + this.props.match.params.id)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('results', result);
+          this.setState({
+          	item : result,
+          	isLoaded: true
+          })
+          console.log(this.state.item.name);
+          // this.SortItems(result.results);
+      },
+      (error) =>{
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      });
   }
 
   render() {
-    return(<img className="loading-image" src={D20} alt="loading"/>)
+  	const {error, item, isLoaded} = this.state;
+  	console.log(this.state);
+  	if(error){
+  		return <div> Error: {error.message}</div>
+  	} else if(!isLoaded){
+  		return <img src={D20} className="loading-image" alt="Loading" />
+  	}else{
+  		return(
+	  		<div className="container">
+	    		<h1 className="monster-name"> {item.name} </h1>
+	    		<h3 className="monster-desc"> {item.desc[0]} </h3> 
+	    	</div>
+  		)
+  	}
   }
 }
 
